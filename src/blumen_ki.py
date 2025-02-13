e3f96930578646cb73801db27a228c9aa44e2a60
roemchen <roms2.0@web.de>
2025-02-13 15:10:52 +0100

    blumen programm fertig

---
+++ src/blumen_ki.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn import datasets
+iris_X, iris_y = datasets.load_iris(return_X_y=True)
+np.unique(iris_y)
+
+# den Datensatz in zwei teile für test und training aufspalten
+np.random.seed(0)
+indices = np.random.permutation(len(iris_X))
+iris_X_train = iris_X[indices[:-10]]
+iris_y_train = iris_y[indices[:-10]]
+iris_X_test = iris_X[indices[-10:]]
+iris_y_test = iris_y[indices[-10:]]
+# Create and fit a nearest-neighbor classifier
+# jede blume als vektor darstellen, und dann den Abstand der Vektoren herausfinden
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(iris_X_train, iris_y_train)
+KNeighborsClassifier()
+print(knn.predict(iris_X_test))
+print(iris_y_test)
